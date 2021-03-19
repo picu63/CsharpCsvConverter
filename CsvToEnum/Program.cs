992359a3d1d308d5d4e141c3bdfdf7783599bbb2
Piotr Olearczyk <picu63@gmail.com>
2021-03-19 16:45:00 +0100

    Add project files.

---
+++ CsvToEnum/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace CsvToEnum
+{
+    class Program
+    {
+        /// <summary>
+        /// 0 - nazwa pliku
+        /// 1 - plik wyjściowy
+        /// 2 - namespace
+        /// 3 - EnumName
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var inputFile = args[0];
+            var outputFile = args[1];
+            var @namespace = args[2];
+            var enumName = args[3];
+            var separator = args[4];
+
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("using System;")
+                .AppendLine()
+                .AppendLine($"namespace {@namespace}")
+                .AppendLine("{")
+                .AppendLine($"\tpublic enum {enumName}")
+                .AppendLine("\t{");
+
+            foreach (var line in File.ReadAllLines(inputFile))
+            {
+                var lineSplitted = line.Split(separator);
+                var enumObjectName = lineSplitted[0];
+                var enumValue = lineSplitted[1];
+                var firstAttribute = lineSplitted[2];
+                var secondAttribute = lineSplitted[3];
+
+                stringBuilder.AppendLine($"\t\t[DomyslnePowiadomienie({firstAttribute}, {secondAttribute})]")
+                    .AppendLine($"\t\t{enumObjectName} = {enumValue},");
+            }
+
+            stringBuilder.AppendLine("\t}").AppendLine("}");
+
+            File.WriteAllText(outputFile, stringBuilder.ToString());
+        }
+    }
+}
---
+++ EnumToCsv/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace EnumToCsv
+{
+    class Program
+    {
+        private const char separator = '|';
+        static void Main(string[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            var allLines = File.ReadAllLines(args[0]);
+            var enumObjectsWithIndex = allLines.Select((line, index) => new Tuple<string, int>(line, index))
+                .Where((tuple) => tuple.Item1.Contains('='));
+            foreach (var o in enumObjectsWithIndex)
+            {
+                var line = o.Item1.Trim();
+                var enumName = line.Split('=')[0].Trim();
+                var enumValue = line.Split('=')[1].Trim().Split(',')[0];
+                var lineIndex = o.Item2;
+                string firstAttribute = string.Empty;
+                string secondAttribute = string.Empty;
+                if (allLines[lineIndex -1].Contains('['))
+                {
+                    var lineWithAttributes = allLines[lineIndex - 1].Trim();
+                    firstAttribute = lineWithAttributes.Substring(lineWithAttributes.IndexOf('(')+1).Split(',')[0];
+                    secondAttribute = lineWithAttributes.Substring(lineWithAttributes.IndexOf(',')+1).Split(')')[0].Trim();
+                }
+
+                stringBuilder.Append(enumName).Append(separator).Append(enumValue).Append(separator).Append(firstAttribute).Append(separator).Append(secondAttribute).AppendLine();
+            }
+            File.WriteAllText(args[1], stringBuilder.ToString());
+        }
+    }
+}
